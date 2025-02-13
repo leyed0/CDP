27f3573c28a624cb3a79383b540474149da8c063
Leyed <orley1994@hotmail.com>
2025-02-12 23:38:11 -0300

    coments

--- Iniciante/C4.cpp
+++ Iniciante/C4.cpp
@@ -2,29 +2,29 @@
 
 using namespace std;
 int main(int argc, char* argv[]){
-    int e, q, it[2] = {0,0};
+    int e, q, it[2] = {0,0}; // e = tamanho da lista - q = tamanho da query - it = iterações para buscas
 
     cin >> e;
 
     int values[100000];
 
-    for(int i=0; i<e; i++)cin >> values[i];
+    for(int i=0; i<e; i++)cin >> values[i]; // reebe lista
 
     cin >> q;
 
     for(int i=0; i<q; i++){
         int qv;
-        cin >> qv;
-        for(int j=0; j<e; j++){
+        cin >> qv; // reebe um mvalor da query
+        for(int j=0; j<e; j++){ //buscar esse valor
             if(qv == values[j]) {
-                it[0]+=j+1;
-                it[1]+=e-j;
+                it[0]+=j+1; //encontrou na busca ->
+                it[1]+=e-j; // calculo para busca <-
                 break;
             }
         }
     }
 
-    cout << it[0] << " " << it[1];
+    cout << it[0] << " " << it[1]; // report
     
     return 0;
 }
